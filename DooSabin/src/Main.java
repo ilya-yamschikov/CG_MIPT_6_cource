54b13a2353f444af843bfa65b980d2f6b9881c37
ilya-yamschikov <ilya.yamschikov@gmail.com>
2013-12-22 02:46:00 +0400

    menu work

--- DooSabin/src/Main.java
+++ DooSabin/src/Main.java
@@ -11,6 +11,17 @@ public class Main {
     private static final int CANVAS_HEIGHT = 480; // height of the drawable
     private static final int FPS = 60; // animator's target frames per second
 
+    private static GridBagConstraints getConstraints(int x, int y) {
+        GridBagConstraints constrains = new GridBagConstraints();
+        constrains.fill = GridBagConstraints.HORIZONTAL;   // компоненты (не) растягиваются
+        constrains.gridx = x;
+        constrains.gridy = y;
+        constrains.insets = new Insets(2, 2, 4, 2);
+        constrains.gridwidth = 1;
+        constrains.anchor = GridBagConstraints.CENTER;    // компонент прижимается к ...
+        return constrains;
+    }
+
     public static void main(String args[]) {
         // Run the GUI codes in the event-dispatching thread for thread safety
         SwingUtilities.invokeLater(new Runnable() {
@@ -28,14 +39,19 @@ public class Main {
 
                 //SETTINGS
                 ImageIcon downButtonIcon = new ImageIcon("down.png");
+                downButtonIcon.setImage(downButtonIcon.getImage().getScaledInstance(24, 20, java.awt.Image.SCALE_SMOOTH));
                 ImageIcon upButtonIcon = new ImageIcon("up.png");
+                upButtonIcon.setImage(upButtonIcon.getImage().getScaledInstance(24,20,java.awt.Image.SCALE_SMOOTH));
                 JPanel settingsPanel = new JPanel();
+                settingsPanel.setLayout(new GridBagLayout());
                 JButton buttonUp = new JButton();
                 buttonUp.setIcon(upButtonIcon);
                 JButton buttonDown = new JButton();
                 buttonDown.setIcon(downButtonIcon);
-                settingsPanel.add(buttonUp);
-                settingsPanel.add(buttonDown);
+                JLabel counter = new JLabel("0");
+                settingsPanel.add(buttonUp, getConstraints(0,0));
+                settingsPanel.add(counter, getConstraints(0,1));
+                settingsPanel.add(buttonDown, getConstraints(0,2));
                 Container pane = frame.getContentPane();
                 pane.add(settingsPanel, BorderLayout.LINE_END);
 
